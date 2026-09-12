909be3c96e4483b74e58c673ba24faed2cc95a49
Shreya S <shreyasharma.1142@gmail.com>
2026-09-13 02:34:11 +0530

    Allow production frontend CORS

--- kanban-backend/backend/server.js
+++ kanban-backend/backend/server.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "http://localhost:5176"
+  "http://localhost:5176",
+  "https://kanban-frontend-q03z.onrender.com"
 ];
 
 app.use(cors({

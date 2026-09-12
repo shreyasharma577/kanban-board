5b92fc29ec08f5f2e989421f6bee9785810fb308
Shreya S <shreyasharma.1142@gmail.com>
2026-09-13 02:15:47 +0530

    Configure production API URL

--- kanban-frontend/src/App.jsx
+++ kanban-frontend/src/App.jsx
@@ -14,7 +14,8 @@ import {
 } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE =
+  import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 const initialTasks = {
   todo: [
